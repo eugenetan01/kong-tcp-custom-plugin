1b6661309043cc834af5e3f9036042d2405757a3
eugenetan01 <eugenetanweihong@gmail.com>
2024-07-04 08:28:29 +0800

    added soap init example

---
+++ soap-rate-limt/readme.md
@@ -0,0 +1,8 @@
+**1. Ensure login creds are as follow:**
+
+- Username: "artemis"
+- password: "artemis"
+
+**2. Command to start the java app**
+
+- `mvn exec:java -Dexec.mainClass="com.example.App"`
---
+++ soap-rate-limt/src/main/java/com/example/App.java
@@ -0,0 +1,183 @@
+package com.example;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import com.example.App.HelloWorldConsumer;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldProducer(), false);
+        Thread.sleep(1000);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldConsumer(), false);
+        thread(new HelloWorldProducer(), false);
+    }
+
+    public static void thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+    }
+
+    public static class HelloWorldProducer implements Runnable {
+        public void run() {
+            try {
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+                connectionFactory.setUserName("artemis");
+                connectionFactory.setPassword("artemis");
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageProducer from the Session to the Topic or Queue
+                MessageProducer producer = session.createProducer(destination);
+                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+                // Create a SOAP message
+                SOAPMessage soapMessage = createSOAPMessage();
+
+                // Convert SOAP message to String
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                soapMessage.writeTo(out);
+                String soapMessageString = new String(out.toByteArray());
+
+                // Create a TextMessage with the SOAP message
+                TextMessage message = session.createTextMessage(soapMessageString);
+
+                // Tell the producer to send the message
+                System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
+                producer.send(message);
+
+                // Clean up
+                session.close();
+                connection.close();
+            }
+            catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        private SOAPMessage createSOAPMessage() throws Exception {
+            MessageFactory messageFactory = MessageFactory.newInstance();
+            SOAPMessage soapMessage = messageFactory.createMessage();
+            SOAPPart soapPart = soapMessage.getSOAPPart();
+
+            // SOAP Envelope
+            SOAPEnvelope envelope = soapPart.getEnvelope();
+            envelope.addNamespaceDeclaration("example", "http://www.example.org");
+
+            // SOAP Body
+            SOAPBody soapBody = envelope.getBody();
+            soapBody.addChildElement("ExampleRequest", "example")
+                    .addChildElement("Message", "example")
+                    .addTextNode("Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode());
+
+            soapMessage.saveChanges();
+
+            // Print the SOAP message
+            System.out.print("Request SOAP Message:");
+            soapMessage.writeTo(System.out);
+            System.out.println();
+            return soapMessage;
+        }
+    }
+
+    public static class HelloWorldConsumer implements Runnable, ExceptionListener {
+        public void run() {
+            try {
+
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+                connectionFactory.setUserName("artemis");
+                connectionFactory.setPassword("artemis");
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageConsumer from the Session to the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait for a message
+                Message message = consumer.receive(1000);
+
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
+}
